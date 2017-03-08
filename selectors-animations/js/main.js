1e4b168329dc71de2c1ad19e637c4ca54519a8d6
AMY ECKERT <amy@amyeckertprojects.com>
2017-03-07 21:11:33 -0500

    class work

--- selectors-animations/js/main.js
+++ selectors-animations/js/main.js
@@ -1,1 +1,18 @@
-;
+
+$(document).ready(function(){
+	// $('[href]').on('click', function(){
+	// 	console.log('clicked a link');
+	// });
+
+	var moveButton = $('.js-move-it');
+	var thingToAnimate = $('.move-this');
+	
+	console.log(thingToAnimate, thingToAnimate2	);
+
+	moveButton.on('click', function(){
+		//thingToAnimate.classList.toggle('is-running');
+		thingToAnimate.toggleClass('is-running');
+	});
+
+
+});// closes doc ready
